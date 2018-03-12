fa1e66a5ab10b9bfc89ee2a6682205eb5c9e1d2f
alekseik1 <alekseik1@yandex.ru>
2018-03-12 11:39:58 +0300

    6 lesson: increment and decrement

---
+++ src/IncrementAndDecrement.kt
@@ -0,0 +1,13 @@
+fun main(args:Array<String>) {
+    var a = 4
+    var b = 2
+
+    //increment
+    var sum = ++a + b
+    //print(sum)
+
+    a = 4
+    //decrement
+    var diff = a - --b
+    print(diff)
+}
