83478a054f069997dbb4b2e80be63347bddf725a
alekseik1 <alekseik1@yandex.ru>
2018-03-12 12:06:43 +0300

    24 lection: arrays

---
+++ src/Arrays.kt
@@ -0,0 +1,10 @@
+fun main(args:Array<String>) {
+    var array = Array<Int>(5){0}
+    array[0] = 10
+    array[1] = 1
+    array[2] = 3
+    array[3] = 5
+    array[4] = 120
+
+    println(array[3])
+}
