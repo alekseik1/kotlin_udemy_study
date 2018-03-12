5e6d21c8aecec1ecbdcd546d9d46a9283a1eea4d
alekseik1 <alekseik1@yandex.ru>
2018-03-12 12:38:07 +0300

    32 lecture: classes -- advanced

--- src/Class.kt
+++ src/Class.kt
@@ -1,10 +1,22 @@
 class Apple(color:String, shape: String) {
+    var color:String? = null
+    var shape:String? = null
+
     init {
-        println("Color: $color")
-        println("Shape: $shape")
+        this.color = color
+        this.shape = shape
+    }
+
+    fun GetColor():String? {
+        return color
+    }
+
+    fun GetShape():String? {
+        return shape
     }
 }
 
 fun main(args:Array<String>) {
     var fruit = Apple("Red", "Circular")
+    print("Color: ${fruit.GetColor()}")
 }
