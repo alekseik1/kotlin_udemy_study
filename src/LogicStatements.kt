3966efd8c657ab652fa59bb36abcbce453ac6405
alekseik1 <alekseik1@yandex.ru>
2018-03-12 11:52:38 +0300

    11 lesson: logic statements

---
+++ src/LogicStatements.kt
@@ -0,0 +1,12 @@
+fun main(args:Array<String>) {
+    // AND &&
+    // OR ||
+    // NOT !
+
+    var a = 50
+    var b = 20
+
+    if(a >= b && a >= 40) {
+        print("a is greater than b and a is greater than 40")
+    }
+}
