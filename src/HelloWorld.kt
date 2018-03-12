df7658f09a3cd2697da049c292568910de76e525
alekseik1 <alekseik1@yandex.ru>
2018-03-12 11:10:23 +0300

    2 lesson: variables

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,4 +1,7 @@
 fun main(args:Array<String>) {
-    println("Hello, World!")
-    print("Welcome to kotlin!")
+    var name = "Aleksei"
+    var age = 18
+    var phone:Int = 123456789
+
+    println("My name is " + name + " and I'm " + age + " years old. My phone number is " + phone)
 }
